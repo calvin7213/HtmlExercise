da1512edb513e7789e582c42d68b5774001768a8
Calvin <calvin7213@gmail.com>
2019-03-10 23:11:58 +0800

    canvas&git.txt

---
+++ canvas/canvas.js
@@ -0,0 +1,5 @@
+window.onload = function(){
+    var canvas = document.getElementById("tshirtCanvas");
+    var context = canvas.getContext("2d");  //在convas中用2D做畫
+    context.fillRect(10, 10, 100, 100);
+}
